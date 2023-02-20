4fc020e28eacb51412f2770815932bbf4a694109
Renaldi Putra <haroro107@gmail.com>
2023-02-20 16:08:46 +0700

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -91,8 +91,6 @@ class ApiController extends Controller
             'message' => 'Could not create token.',
          ], 500);
       }
-
-      //Token created, return with success response and jwt token
       return response()->json([
          'success' => true,
          'token' => $token,
